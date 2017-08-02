62f6662db3e36858bca475cf08c8df55a56879d7
Rafal Chojna <rafalc@wolfram.com>
2017-08-02 18:40:13 +0200

    In Image: add copy and move constructors, copy and move assignment operators, getter and setter for specific channel value in Image plus some other functions that are already present in Tensor and RawArray.

--- src/Image.h
+++ src/Image.h
@@ -69,6 +69,48 @@ namespace LibraryLinkUtils {
 		 **/
 		Image(const MImage mi);
 
+
+		/**
+		 *   @brief         Copy constructor with type conversion
+		 *   @tparam		U - any type that Image supports
+		 *   @param[in]     t2 - const reference to an Image
+		 **/
+		template<typename U>
+		Image(const Image<U>& t2);
+
+		/**
+		 *   @brief         Copy constructor with type conversion and explicitly specified interleaving
+		 *   @tparam		U - any type that Image supports
+		 *   @param[in]     t2 - const reference to an Image
+		 *   @param[in]		interleavedQ - whether the newly created Image should be interleaved
+		 **/
+		template<typename U>
+		Image(const Image<U>& t2, bool interleavedQ);
+
+		/**
+		 *   @brief         Copy constructor
+		 *   @param[in]     t2 - const reference to Image of matching type
+		 **/
+		Image(const Image<T>& t2);
+
+		/**
+		 *   @brief         Move constructor
+		 *   @param[in]     t2 - rvalue reference to Image of matching type
+		 **/
+		Image(Image<T>&& t2);
+
+		/**
+		 *   @brief         Copy-assignment operator
+		 *   @param[in]     t2 - const reference to Image of matching type
+		 **/
+		void operator=(const Image<T>& t2);
+
+		/**
+		 *   @brief         Move-assignment operator
+		 *   @param[in]     t2 - rvalue reference to Image of matching type
+		 **/
+		void operator=(Image<T>&& t2);
+
 		/**
 		 *   @brief	Free internal MImage if necessary
 		 **/
@@ -137,6 +179,79 @@ namespace LibraryLinkUtils {
 			return allowSlices;
 		}
 
+
+		/**
+		 *   @brief         Get channel value at specified position in 2D image
+		 *   @param[in]     row - pixel row (in Mathematica-style indexing - starting from 1)
+		 *   @param[in]     col - pixel column (in Mathematica-style indexing - starting from 1)
+		 *   @param[in]     channel - desired channel (in Mathematica-style indexing - starting from 1)
+		 *   @throws		LLErrorCode::ImageIndexError - if the specified coordinates are out-of-bound
+		 **/
+		T get(mint row, mint col, mint channel) const;
+
+		/**
+		 *   @brief         Get channel value at specified position in 3D image
+		 *   @param[in]		slice - slice index (in Mathematica-style indexing - starting from 1)
+		 *   @param[in]     row - pixel row (in Mathematica-style indexing - starting from 1)
+		 *   @param[in]     col - pixel column (in Mathematica-style indexing - starting from 1)
+		 *   @param[in]     channel - desired channel (in Mathematica-style indexing - starting from 1)
+		 *   @throws		LLErrorCode::ImageIndexError - if the specified coordinates are out-of-bound
+		 **/
+		T get(mint slice, mint row, mint col, mint channel) const;
+
+		/**
+		 *   @brief         Set channel value at specified position in 2D image
+		 *   @param[in]     row - pixel row (in Mathematica-style indexing - starting from 1)
+		 *   @param[in]     col - pixel column (in Mathematica-style indexing - starting from 1)
+		 *   @param[in]     channel - desired channel (in Mathematica-style indexing - starting from 1)
+		 *   @throws		LLErrorCode::ImageIndexError - if the specified coordinates are out-of-bound
+		 **/
+		void set(mint row, mint col, mint channel, T newValue);
+
+		/**
+		 *   @brief         Set channel value at specified position in 3D image
+		 *   @param[in]		slice - slice index (in Mathematica-style indexing - starting from 1)
+		 *   @param[in]     row - pixel row (in Mathematica-style indexing - starting from 1)
+		 *   @param[in]     col - pixel column (in Mathematica-style indexing - starting from 1)
+		 *   @param[in]     channel - desired channel (in Mathematica-style indexing - starting from 1)
+		 *   @throws		LLErrorCode::ImageIndexError - if the specified coordinates are out-of-bound
+		 **/
+		void set(mint slice, mint row, mint col, mint channel, T newValue);
+
+		/**
+		 *   @brief 	Return internal MImage. You should rarely need this function.
+		 *   @warning 	Do not modify MImage returned from this function!
+		 **/
+		MImage getInternal() const noexcept {
+			return internalMI;
+		}
+
+		/**
+		 *   @brief 	Return share count of internal MImage.
+		 *   Use this to manually manage shared MImages.
+		 *
+		 *   @see 		<http://reference.wolfram.com/language/LibraryLink/ref/callback/MImage_shareCount.html>
+		 *   @note		LibraryLink does not provide any way to check whether MImage was passed or will be returned as "Shared".
+		 *   Therefore passing or returning MImages as "Shared" is discouraged and if you do that you are responsible for managing MImage memory.
+		 **/
+		mint shareCount() const noexcept {
+			if (internalMI == nullptr)
+				return 0;
+			return this->imgFuns->MImage_shareCount(internalMI);
+		}
+
+		/**
+		 *   @brief 	Disown internal MImage that is shared with Mathematica.
+		 *   Use this to manually manage shared MImages.
+		 *
+		 *   @see 		<http://reference.wolfram.com/language/LibraryLink/ref/callback/MImage_disown.html>
+		 *   @note		LibraryLink does not provide any way to check whether MImage was passed or will be returned as "Shared".
+		 *   Therefore passing or returning MImages as "Shared" is discouraged and if you do that you are responsible for managing MImage memory.
+		 **/
+		void disown() const noexcept {
+			if (internalMI)
+				this->imgFuns->MImage_disown(internalMI);
+		}
 	private:
 		/**
 		 *   @brief Return raw pointer to underlying image data
@@ -185,7 +300,12 @@ namespace LibraryLinkUtils {
 		 *   @brief 	Sub-class implementation of virtual void MArray<T>::freeInternal(). It frees internal MImage.
 		 **/
 		void freeInternal() noexcept override {
-			this->imgFuns->MImage_free(internalMI);
+			if (internalMI == nullptr)
+				return;
+			if (shareCount() > 0)
+				disown();
+			if (this->arrayOwnerQ)
+				this->imgFuns->MImage_free(internalMI);
 		}
 
 		/**
@@ -204,7 +324,7 @@ namespace LibraryLinkUtils {
 		MImage internalMI = nullptr;
 
 		/// Whether internal image is 2D or 3D
-		bool allowSlices;
+		bool allowSlices = false;
 	};
 
 	template<typename T>
@@ -236,10 +356,60 @@ namespace LibraryLinkUtils {
 	}
 
 	template<typename T>
+	template<typename U>
+	Image<T>::Image(const Image<U>& i2) : Image<T>(i2, i2.interleavedQ()) {
+	}
+
+	template<typename T>
+	template<typename U>
+	Image<T>::Image(const Image<U>& i2, bool interleavedQ) : MArray<T>(i2) {
+		internalMI = imgFuns->MImage_convertType(i2.getInternal(), type, interleavedQ);
+		allowSlices = i2.is3D();
+	}
+
+	template<typename T>
 	Image<T>::~Image() {
 		if (this->arrayOwnerQ)
 			this->freeInternal();
 	}
+
+	template<typename T>
+	Image<T>::Image(const Image<T>& i2) : MArray<T>(i2) {
+		allowSlices = i2.allowSlices;
+		if (this->imgFuns->MImage_clone(i2.internalMI, &this->internalMI)) {
+			throw LibraryLinkError(LLErrorCode::ImageCloneError);
+		}
+		this->arrayOwnerQ = true;
+	}
+
+	template<typename T>
+	Image<T>::Image(Image<T>&& i2) : MArray<T>(std::move(i2)) {
+		allowSlices = i2.allowSlices;
+		this->internalMI = i2.internalMI;
+		i2.internalMI = nullptr;
+		i2.arrayOwnerQ = false;
+	}
+
+	template<typename T>
+	void Image<T>::operator=(const Image<T>& i2) {
+		MArray<T>::operator=(i2);
+		this->freeInternal();
+		if (this->imgFuns->MImage_clone(i2.internalMI, &this->internalMI)) {
+			throw LibraryLinkError(LLErrorCode::ImageCloneError);
+		}
+		this->arrayOwnerQ = true;
+	}
+
+	template<typename T>
+	void Image<T>::operator=(Image<T>&& i2) {
+		MArray<T>::operator=(std::move(i2));
+		this->freeInternal();
+		this->internalMI = i2.internalMI;
+		this->arrayOwnerQ = i2.arrayOwnerQ;
+		i2.internalMI = nullptr;
+		i2.arrayOwnerQ = false;
+	}
+
 
 	template<typename T>
 	void Image<T>::initDataMembers() {
