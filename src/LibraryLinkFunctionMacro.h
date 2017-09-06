a7d96c8f06989d950d2c062535b36aec9b64f8d3
Rafal Chojna <rafalc@wolfram.com>
2017-09-06 16:02:03 +0200

    Add header guards

--- src/LibraryLinkFunctionMacro.h
+++ src/LibraryLinkFunctionMacro.h
@@ -10,7 +10,11 @@
  * @see		https://stackoverflow.com/questions/45590594/generic-lambda-in-extern-c-function
  */
 
+#ifndef LLUTILS_LIBRARYLINKFUNCTIONMACRO_H_
+#define LLUTILS_LIBRARYLINKFUNCTIONMACRO_H_
+
 #define LIBRARY_LINK_FUNCTION(name) \
 		EXTERN_C DLLEXPORT int name(WolframLibraryData libData, mint Argc, MArgument *Args, MArgument Res); \
 		int name(WolframLibraryData libData, mint Argc, MArgument *Args, MArgument Res)
 
+#endif /* LLUTILS_LIBRARYLINKFUNCTIONMACRO_H_ */
