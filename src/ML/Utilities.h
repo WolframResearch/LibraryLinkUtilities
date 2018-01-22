a2923f4d46d61e76a1e49a7d9452d95dcbb686a9
Rafal Chojna <rafalc@wolfram.com>
2018-01-22 21:15:37 +0100

    Add doxygen comments

--- src/ML/Utilities.h
+++ src/ML/Utilities.h
@@ -19,35 +19,113 @@ namespace LibraryLinkUtils {
 
 	namespace ML {
 
+		/**
+		 * @brief 		Checks if MathLink operation was successful and throws appropriate exception otherwise
+		 * @param[in] 	m - low-level object of type MLINK received from LibraryLink
+		 * @param[in] 	statusOk - status code return from a MathLink function
+		 * @param[in] 	errorCode - which error code to put in the exception if MathLink function failed
+		 * @param[in] 	debugInfo - additional info to be attached to the exception
+		 */
 		void checkError(MLINK m, int statusOk, int errorCode, const std::string& debugInfo = "");
+
+		/**
+		 * @brief 		Checks if MathLink operation was successful and throws appropriate exception otherwise
+		 * @param[in] 	m - low-level object of type MLINK received from LibraryLink
+		 * @param[in] 	statusOk - status code return from a MathLink function
+		 * @param[in] 	errorName - what error name to put in the exception if MathLink function failed
+		 * @param[in] 	debugInfo - additional info to be attached to the exception
+		 */
 		void checkError(MLINK m, int statusOk, const std::string& errorName, const std::string& debugInfo = "");
 
+		/**
+		 * @struct 	Symbol
+		 * @brief	Structure representing any symbol in Wolfram Language
+		 */
 		struct Symbol {
+			/**
+			 * @brief Default constructor.
+			 * The head is empty and such Symbol can be used as argument of MathLinkStream::operator>>, so to read a Symbol from top-level.
+			 */
 			Symbol() = default;
+
+			/**
+			 * @brief	Constructs a Symbol given its head.
+			 * @param 	h - head (or Symbol's name)
+			 */
 			explicit Symbol(std::string h) : head(std::move(h)) {};
 
+			/**
+			 * Get Symbol name/head.
+			 * @return head
+			 */
 			const std::string& getHead() const;
+
+			/**
+			 * Set Symbol name/head.
+			 * @param h - new head for the Symbol
+			 */
 			void setHead(std::string h);
+
 		private:
 			std::string head;
 		};
 
+		/**
+		 * @struct 	Function
+		 * @brief	Structure representing any function in Wolfram Language, i.e. a head plus number of arguments.
+		 */
 		struct Function : Symbol {
+
+			/**
+			 * @brief Default constructor.
+			 * The head is empty and number of arguments is set to a dummy value.
+			 * This constructor should only be used to create a Function right before calling MathLinkStream::operator>> on it, which will read a function from top-level.
+			 */
 			Function() : Function("", -1) {};
+
+			/**
+			 * @brief Create a Function with given head but unknown number of arguments.
+			 * You can later call MathLinkStream::operator>> on such constructed Function to populate argument count with a value read from top-level.
+			 * @param h - function head
+			 */
 			explicit Function(const std::string& h) : Function(h, -1) {}
+
+			/**
+			 * @brief	Construct a Function with given head and number of arguments
+			 * @param 	h - function head
+			 * @param 	argCount - number of arguments this function takes
+			 */
 			Function(const std::string& h, int argCount) : Symbol(h), argc(argCount) {}
 
+			/**
+			 * @brief	Get argument count.
+			 * @return 	number of arguments this function takes
+			 */
 			int getArgc() const;
+
+			/**
+			 * @brief Set argument count.
+			 * @param newArgc - new value for argument count
+			 */
 			void setArgc(int newArgc);
+
 		private:
 			int argc;
 		};
 
+		/**
+		 * @struct	Association
+		 * Special case of a Function.
+		 */
 		struct Association : Function {
 			Association() : Function("Association") {}
 			Association(int argCount) : Function("Association", argCount) {}
 		};
 
+		/**
+		 * @struct	List
+		 * Special case of a Function.
+		 */
 		struct List : Function {
 			List() : Function("List") {}
 			List(int argCount) : Function("List", argCount) {}
