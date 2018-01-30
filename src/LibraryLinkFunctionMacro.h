a92475890aeac81eb50fd1ffb3394640f45dd306
Rafal Chojna <rafalc@wolfram.com>
2018-01-30 10:51:09 +0100

    Add LIBRARY_MATHLINK_FUNCTION macro to LibraryLinkFunctionMacro.h

--- src/LibraryLinkFunctionMacro.h
+++ src/LibraryLinkFunctionMacro.h
@@ -3,7 +3,7 @@
  * @author	Rafal Chojna <rafalc@wolfram.com>
  * @date	10/08/2017
  *
- * @brief	Macro designed to solve a MSVC bug concerning extern "C" and generic lambdas.
+ * @brief	Macros designed to workaround a MSVC bug concerning extern "C" and generic lambdas.
  * 			Defined in a separate header file to limit it's scope.
  * 			Use this macro only for interface functions (functions that will be loaded with LibraryFunctionLoad).
  *
@@ -14,7 +14,11 @@
 #define LLUTILS_LIBRARYLINKFUNCTIONMACRO_H_
 
 #define LIBRARY_LINK_FUNCTION(name) \
-		EXTERN_C DLLEXPORT int name(WolframLibraryData libData, mint Argc, MArgument *Args, MArgument Res); \
+		EXTERN_C DLLEXPORT int name(WolframLibraryData, mint, MArgument*, MArgument); \
 		int name(WolframLibraryData libData, mint Argc, MArgument *Args, MArgument Res)
+
+#define LIBRARY_MATHLINK_FUNCTION(name) \
+		EXTERN_C DLLEXPORT int name(WolframLibraryData, MLINK); \
+		int name(WolframLibraryData libData, MLINK mlp)
 
 #endif /* LLUTILS_LIBRARYLINKFUNCTIONMACRO_H_ */
