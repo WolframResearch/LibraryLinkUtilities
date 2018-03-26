2cd6ea47c4c37170a8fbab6229568e11048f32b7
Rafal Chojna <rafalc@wolfram.com>
2018-03-26 12:25:16 +0200

    Add new errors related to string encoding

--- src/LibraryLinkError.cpp
+++ src/LibraryLinkError.cpp
@@ -84,7 +84,9 @@ namespace LibraryLinkUtils {
 			{ LLErrorName::MLGetSymbolError,		"MLGetSymbol failed." },
 			{ LLErrorName::MLGetFunctionError,		"MLGetFunction failed." },
 			{ LLErrorName::MLPacketHandleError,		"One of the packet handling functions failed." },
-			{ LLErrorName::MLFlowControlError,		"One of the flow control functions failed." }
+			{ LLErrorName::MLFlowControlError,		"One of the flow control functions failed." },
+			{ LLErrorName::MLInvalidInEncoding,		"Encoding specified for reading strings from Mathematica is invalid." },
+			{ LLErrorName::MLInvalidOutEncoding,	"Encoding specified for writing strings to Mathematica is invalid." }
 		});
 		return errMap;
 	}
@@ -231,6 +233,8 @@ namespace LibraryLinkUtils {
 		LLU_DEFINE_ERROR_NAME(MLGetFunctionError);
 		LLU_DEFINE_ERROR_NAME(MLPacketHandleError);
 		LLU_DEFINE_ERROR_NAME(MLFlowControlError);
+		LLU_DEFINE_ERROR_NAME(MLInvalidInEncoding);
+		LLU_DEFINE_ERROR_NAME(MLInvalidOutEncoding);
 	}
 
 } /* namespace LibraryLinkUtils */
