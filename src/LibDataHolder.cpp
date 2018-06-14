dad948a8c50664b42b4f69a89275e8ddb379983b
Rafal Chojna <rafalc@wolfram.com>
2018-06-14 11:34:04 +0200

    Implement LibDataHolder

--- src/LibDataHolder.cpp
+++ src/LibDataHolder.cpp
@@ -1,4 +1,28 @@
-//
-// Created by rafal on 14.06.18.
-//
+/**
+ * @file	LibDataHolder.h
+ * @author	Rafal Chojna <rafalc@wolfram.com>
+ * @date	14/06/2018
+ *
+ * @brief	Implementation of the LibDataHolder class.
+ *
+ */
 
+#include "LibDataHolder.h"
+
+namespace LibraryLinkUtils {
+
+	std::unique_ptr<st_WolframLibraryData> LibDataHolder::libData;
+	WolframRawArrayLibrary_Functions LibDataHolder::raFuns = nullptr;
+	WolframImageLibrary_Functions LibDataHolder::imgFuns = nullptr;
+
+	void LibDataHolder::setLibraryData(WolframLibraryData ld) {
+		libData = ld? std::make_unique<st_WolframLibraryData>(*ld) : nullptr;
+		raFuns = ld? libData->rawarrayLibraryFunctions : nullptr;
+		imgFuns = ld? libData->imageLibraryFunctions : nullptr;
+	}
+
+	WolframLibraryData LibDataHolder::getLibraryData() noexcept {
+		return libData.get();
+	}
+
+}
--- src/LibDataHolder.h
+++ src/LibDataHolder.h
@@ -1,8 +1,46 @@
-//
-// Created by rafal on 14.06.18.
-//
+/**
+ * @file	LibDataHolder.h
+ * @author	Rafal Chojna <rafalc@wolfram.com>
+ * @date	14/06/2018
+ *
+ * @brief	Definition of the LibDataHolder class.
+ *
+ */
+#ifndef LLUTILS_LIBDATAHOLDER_H
+#define LLUTILS_LIBDATAHOLDER_H
 
-#ifndef LIBRARYLINKUTILITIES_LIBDATAHOLDER_H
-#define LIBRARYLINKUTILITIES_LIBDATAHOLDER_H
+#include <memory>
 
-#endif //LIBRARYLINKUTILITIES_LIBDATAHOLDER_H
+#include "WolframLibrary.h"
+#include "WolframImageLibrary.h"
+#include "WolframRawArrayLibrary.h"
+
+namespace LibraryLinkUtils {
+
+	/**
+	 * @struct 	LibDataHolder
+	 * @brief	This structure offers a static copy of WolframLibData accessible throughout the whole life of the DLL.
+	 */
+	struct LibDataHolder {
+		/**
+		 *   @brief         Set WolframLibraryData structure as static member of LibDataHolder. Call this function in WolframLibrary_initialize.
+		 *   @param[in]     ld - WolframLibraryData passed to every library function via LibraryLink
+		 *   @warning		This function must be called before constructing the first MArgumentManager unless you use a constructor that takes WolframLibraryData as argument
+		 **/
+		static void setLibraryData(WolframLibraryData ld);
+
+		/**
+		 *   @brief         Get currently owned WolframLibraryData.
+		 *   @return     	a non-owning pointer to current instance of st_WolframLibraryData stored in LibDataHolder or nullptr in case there is none
+		 **/
+		static WolframLibraryData getLibraryData() noexcept;
+
+	protected:
+		static std::unique_ptr<st_WolframLibraryData> libData;
+		static WolframRawArrayLibrary_Functions raFuns;
+		static WolframImageLibrary_Functions imgFuns;
+	};
+
+}
+
+#endif //LLUTILS_LIBDATAHOLDER_H
