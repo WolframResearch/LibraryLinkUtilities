1e80c14dbe6f9566507ccc0d7bb83f769d3214dd
Rafal Chojna <rafalc@wolfram.com>
2018-08-21 18:24:59 +0200

    Adjust existing unit test for strings and add more.

--- tests/UnitTests/StringOperations/EchoString/EchoString.cpp
+++ tests/UnitTests/StringOperations/EchoString/EchoString.cpp
@@ -4,8 +4,8 @@
 EXTERN_C DLLEXPORT int EchoString(WolframLibraryData libData, mint Argc, MArgument *Args, MArgument Res) {
 	LibraryLinkUtils::MArgumentManager mngr(libData, Argc, Args, Res);
 
-	auto& in1 = mngr.getString(0);
-	mngr.setString(std::move(in1));
+	auto* in1 = mngr.getCString(0);
+	mngr.setString(in1);
 
 	return LIBRARY_NO_ERROR;
 }
--- tests/UnitTests/StringOperations/Greetings/Greetings.cpp
+++ tests/UnitTests/StringOperations/Greetings/Greetings.cpp
@@ -5,7 +5,7 @@
 EXTERN_C DLLEXPORT int Greetings(WolframLibraryData libData, mint Argc, MArgument *Args, MArgument Res) {
 	LibraryLinkUtils::MArgumentManager mngr(libData, Argc, Args, Res);
 
-	auto& name = mngr.getString(0);
+	auto name = mngr.getString(0);
 	mngr.setString(std::string("Greetings ") + name + "!");
 	return LIBRARY_NO_ERROR;
 }
