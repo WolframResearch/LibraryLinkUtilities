86fb9f23a8196098ac6456e0f3f628d562039ad3
Rafal Chojna <rafalc@wolfram.com>
2018-11-18 20:13:02 +0100

    Initial implementation of ProgressMonitor.

---
+++ src/ProgressMonitor.cpp
@@ -0,0 +1,67 @@
+/**
+ * @file	ProgressMonitor.cpp
+ * @author	Rafal Chojna <rafalc@wolfram.com>
+ * @brief	
+ */
+#include "LLU/ProgressMonitor.h"
+
+#include "LLU/Containers/LibDataHolder.h"
+#include "LLU/LibraryLinkError.h"
+
+namespace LibraryLinkUtils {
+
+	ProgressMonitor::ProgressMonitor(Tensor<double> sharedIndicator, double step) : sharedIndicator(std::move(sharedIndicator)), step(step) {}
+
+	ProgressMonitor::~ProgressMonitor() {
+		sharedIndicator.disown();
+	}
+
+	double ProgressMonitor::get() const {
+		return sharedIndicator[0];
+	}
+
+	void ProgressMonitor::set(double progressValue) {
+		sharedIndicator[0] = progressValue;
+		checkAbort();
+	}
+
+	double ProgressMonitor::getStep() const {
+		return step;
+	}
+
+	void ProgressMonitor::setStep(double stepValue) {
+		step = stepValue;
+		checkAbort();
+	}
+
+	void ProgressMonitor::checkAbort() const {
+		auto libData = LibDataHolder::getLibraryData();
+		if (!libData) {
+			ErrorManager::throwException(LLErrorName::MArgumentLibDataError);
+		}
+		if(libData->AbortQ()) {
+			ErrorManager::throwException(LLErrorName::Aborted);
+		}
+	}
+
+	ProgressMonitor& ProgressMonitor::operator++() {
+		set(sharedIndicator[0] + step);
+		return *this;
+	}
+
+	ProgressMonitor& ProgressMonitor::operator +=(double progress) {
+		set(sharedIndicator[0] + progress);
+		return *this;
+	}
+
+	ProgressMonitor& ProgressMonitor::operator--() {
+		set(sharedIndicator[0] - step);
+		return *this;
+	}
+
+	ProgressMonitor& ProgressMonitor::operator -=(double progress) {
+		set(sharedIndicator[0] - progress);
+		return *this;
+	}
+
+}
