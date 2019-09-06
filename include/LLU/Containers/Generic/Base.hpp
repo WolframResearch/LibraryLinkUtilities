b483a0847b7803ee01e40f2509489c640cf71a10
Rafal Chojna <rafalc@wolfram.com>
2019-09-06 22:43:48 +0200

    Remove PassingMode alias from MContainerBase.

--- include/LLU/Containers/Generic/Base.hpp
+++ include/LLU/Containers/Generic/Base.hpp
@@ -26,7 +26,6 @@ namespace LLU {
 	class MContainerBase : public PassingMode {
 	public:
 		using Container = MType_t<Type>;
-		using Passing = PassingMode;
 	public:
 		/**
 		 * @brief Default constructor.
