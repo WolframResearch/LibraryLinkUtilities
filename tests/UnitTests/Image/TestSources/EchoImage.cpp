e0dfedbe1047ac8142a7079662c624ea347ced65
Rafal Chojna <rafalc@wolfram.com>
2019-09-16 17:39:45 +0200

    Add new macro LLU_LIBRARY_FUNCTION that contains all the boilerplate code used often in library functions. Use this macro in Image unit tests.

--- tests/UnitTests/Image/TestSources/EchoImage.cpp
+++ tests/UnitTests/Image/TestSources/EchoImage.cpp
@@ -6,151 +6,76 @@
 
 namespace LLErrorCode = LLU::ErrorCode;
 
-LIBRARY_LINK_FUNCTION(EchoImage1) {
-	auto err = LLErrorCode::NoError;
-	try {
-		LLU::MArgumentManager mngr(libData, Argc, Args, Res);
-		mngr.operateOnImage(0, [&mngr](auto im1) {
-			using T = typename std::remove_reference_t<decltype(im1)>::value_type;
-			auto im2 {std::move(im1)};  // test move constructor
-			LLU::Image<T, LLU::Passing::Manual> im3;
-			im3 = std::move(im2);  // test move assignment
-			mngr.setImage(im3);
-		});
-	}
-	catch (const LLU::LibraryLinkError& e) {
-		err = e.which();
-	}
-	catch (...) {
-		err = LLErrorCode::FunctionError;
-	}
-	return err;
+LLU_LIBRARY_FUNCTION(EchoImage1) {
+	mngr.operateOnImage(0, [&mngr](auto im1) {
+		using T = typename std::remove_reference_t<decltype(im1)>::value_type;
+		auto im2 {std::move(im1)};  // test move constructor
+		LLU::Image<T, LLU::Passing::Manual> im3;
+		im3 = std::move(im2);  // test move assignment
+		mngr.setImage(im3);
+	});
 }
 
-LIBRARY_LINK_FUNCTION(EchoImage2) {
-	auto err = LLErrorCode::NoError;
-	try {
-		LLU::MArgumentManager mngr(libData, Argc, Args, Res);
+LLU_LIBRARY_FUNCTION(EchoImage2) {
+	mngr.operateOnImage(0, [&mngr](auto&& in) {
+		auto slices = in.is3D()? in.slices() : 0;
+		auto columns = in.columns();
+		auto rows = in.rows();
+		auto channels = in.channels();
+		auto colorspace = in.colorspace();
+		auto interleaving = in.interleavedQ();
 
-		mngr.operateOnImage(0, [&mngr](auto&& in) {
-			auto slices = in.is3D()? in.slices() : 0;
-			auto columns = in.columns();
-			auto rows = in.rows();
-			auto channels = in.channels();
-			auto colorspace = in.colorspace();
-			auto interleaving = in.interleavedQ();
+		using T = typename std::remove_reference_t<decltype(in)>::value_type;
+		LLU::Image<T> out(slices, columns, rows, channels, colorspace, interleaving);
 
-			using T = typename std::remove_reference_t<decltype(in)>::value_type;
-			LLU::Image<T> out(slices, columns, rows, channels, colorspace, interleaving);
-			
-			for (mint column = 1; column <= columns; column++) {
-				for (mint row = 1; row <= rows; row++) {
-					if (in.is3D()) {
-						for (mint slice = 1; slice <= slices; slice++) {
-							out.set(slice, row, column, 1, in.get(slice, row, column, 1));
-						}
-					} else {
-						out.set(row, column, 1, in.get(row, column, 1));
+		for (mint column = 1; column <= columns; column++) {
+			for (mint row = 1; row <= rows; row++) {
+				if (in.is3D()) {
+					for (mint slice = 1; slice <= slices; slice++) {
+						out.set(slice, row, column, 1, in.get(slice, row, column, 1));
 					}
+				} else {
+					out.set(row, column, 1, in.get(row, column, 1));
 				}
 			}
+		}
+		mngr.setImage(out);
+	});
+}
+
+LLU_LIBRARY_FUNCTION(EchoImage3) {
+	auto img = mngr.getGenericImage(0);
+	mngr.set(img);
+}
+
+LLU_LIBRARY_FUNCTION(ConvertImageToByte) {
+	mngr.operateOnImage(0, [&mngr](auto&& in) {
+		LLU::Image<std::uint8_t> out(in);
+		mngr.setImage(out);
+	});
+}
+
+LLU_LIBRARY_FUNCTION(UnifyImageTypes) {
+	mngr.operateOnImage(0, [&mngr](auto&& in) {
+		using T = typename std::remove_reference_t<decltype(in)>::value_type;
+		mngr.operateOnImage(1, [&mngr](auto&& in2) {
+			LLU::Image<T> out(in2);
 			mngr.setImage(out);
 		});
-	}
-	catch (const LLU::LibraryLinkError& e) {
-		err = e.which();
-	}
-	catch (...) {
-		err = LLErrorCode::FunctionError;
-	}
-	return err;
+
+	});
 }
 
-LIBRARY_LINK_FUNCTION(EchoImage3) {
-	auto err = LLErrorCode::NoError;
-	try {
-		LLU::MArgumentManager mngr(libData, Argc, Args, Res);
-		auto img = mngr.getGenericImage(0);
-		mngr.set(img);
-	}
-	catch (const LLU::LibraryLinkError &e) {
-		err = e.which();
-	}
-	return err;
+LLU_LIBRARY_FUNCTION(CloneImage) {
+	mngr.operateOnImage(0, [&mngr](auto&& im1) {
+		using T = typename std::remove_reference_t<decltype(im1)>::value_type;
+		LLU::Image<T, LLU::Passing::Manual> im2 {im1};  // test copy constructor
+		LLU::Image<T> im3;
+		im3 = im2;  // test copy assignment
+		mngr.setImage(im3);
+	});
 }
 
-LIBRARY_LINK_FUNCTION(ConvertImageToByte) {
-	auto err = LLErrorCode::NoError;
-	try {
-		LLU::MArgumentManager mngr(libData, Argc, Args, Res);
-		mngr.operateOnImage(0, [&mngr](auto&& in) {
-			LLU::Image<std::uint8_t> out(in);
-			mngr.setImage(out);
-		});
-	}
-	catch (const LLU::LibraryLinkError& e) {
-		err = e.which();
-	}
-	catch (...) {
-		err = LLErrorCode::FunctionError;
-	}
-	return err;
-}
-
-LIBRARY_LINK_FUNCTION(UnifyImageTypes) {
-	auto err = LLErrorCode::NoError;
-	try {
-		LLU::MArgumentManager mngr(libData, Argc, Args, Res);
-		mngr.operateOnImage(0, [&mngr](auto&& in) {
-			using T = typename std::remove_reference_t<decltype(in)>::value_type;
-			mngr.operateOnImage(1, [&mngr](auto&& in2) {
-				LLU::Image<T> out(in2);
-				mngr.setImage(out);
-			});
-
-		});
-	}
-	catch (const LLU::LibraryLinkError& e) {
-		err = e.which();
-	}
-	catch (...) {
-		err = LLErrorCode::FunctionError;
-	}
-	return err;
-}
-
-LIBRARY_LINK_FUNCTION(CloneImage) {
-	auto err = LLErrorCode::NoError;
-	try {
-		LLU::MArgumentManager mngr(libData, Argc, Args, Res);
-		mngr.operateOnImage(0, [&mngr](auto&& im1) {
-			using T = typename std::remove_reference_t<decltype(im1)>::value_type;
-			LLU::Image<T, LLU::Passing::Manual> im2 {im1};  // test copy constructor
-			LLU::Image<T> im3;
-			im3 = im2;  // test copy assignment
-			mngr.setImage(im3);
-		});
-	}
-	catch (const LLU::LibraryLinkError &e) {
-		err = e.which();
-	}
-	catch (...) {
-		err = LLErrorCode::FunctionError;
-	}
-	return err;
-}
-
-LIBRARY_LINK_FUNCTION(EmptyWrapper) {
-	auto err = LLErrorCode::NoError;
-	try {
-		LLU::MArgumentManager mngr(libData, Argc, Args, Res);
-		LLU::Image<std::uint8_t> im { nullptr }; // this should trigger an exception
-	}
-	catch (const LLU::LibraryLinkError &e) {
-		err = e.which();
-	}
-	catch (...) {
-		err = LLErrorCode::FunctionError;
-	}
-	return err;
+LLU_LIBRARY_FUNCTION(EmptyWrapper) {
+	LLU::Image<std::uint8_t> im { nullptr }; // this should trigger an exception
 }
