1ab001ffb5bb2f9652014ba073d7fe43baad6afe
Rafal Chojna <rafalc@wolfram.com>
2019-10-02 20:18:05 +0200

    Move image assets to separate directory.

--- docs/conf.py
+++ docs/conf.py
@@ -135,12 +135,12 @@ html_theme_path = [sphinx_theme.get_html_theme_path('neo_rtd_theme')]
 
 # The name of an image file (relative to this directory) to place at the top
 # of the sidebar.
-html_logo = '_static/LLULogo.png'
+html_logo = '_static/img/LLULogo.png'
 
 # The name of an image file (relative to this directory) to use as a favicon of
 # the docs.  This file should be a Windows icon file (.ico) being 16x16 or 32x32
 # pixels large.
-html_favicon = '_static/favicon-32.png'
+html_favicon = '_static/img/favicon-32.png'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
