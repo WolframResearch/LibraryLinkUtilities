182145c5d28219aafb0bb53cce064ba1a621b245
Rafal Chojna <rafalc@wolfram.com>
2019-11-29 18:18:35 +0100

    Use static class members to implement ImageType, TensorType and NumericArrayType because there are no inline variables in C++14 so it's easier that way.

--- include/LLU/Utilities.hpp
+++ include/LLU/Utilities.hpp
@@ -221,66 +221,120 @@ namespace LLU {
 		}
 	}
 
-	/// Utility structure that matches a C++ type with a corresponding MImage data type
+	/// @cond
 	template<typename T>
-	constexpr imagedata_t ImageType = MImage_Type_Undef;
+	struct ImageTypeImpl {
+		static constexpr imagedata_t type = MImage_Type_Undef;
+	};
+	template<>
+	struct ImageTypeImpl<int8_t> {
+		static constexpr imagedata_t type = MImage_Type_Bit;
+	};
+	template<>
+	struct ImageTypeImpl<uint8_t> {
+		static constexpr imagedata_t type = MImage_Type_Bit8;
+	};
+	template<>
+	struct ImageTypeImpl<uint16_t> {
+		static constexpr imagedata_t type = MImage_Type_Bit16;
+	};
+	template<>
+	struct ImageTypeImpl<float> {
+		static constexpr imagedata_t type = MImage_Type_Real32;
+	};
+	template<>
+	struct ImageTypeImpl<double> {
+		static constexpr imagedata_t type = MImage_Type_Real;
+	};
+	/// @endcond
+
+
+	/// Utility variable template that matches a C++ type with a corresponding MImage data type
+	template<typename T>
+	constexpr imagedata_t ImageType = ImageTypeImpl<T>::type;
 
 	/// @cond
+	template<typename T>
+	struct NumericArrayTypeImpl {
+		static constexpr numericarray_data_t type = MNumericArray_Type_Undef;
+	};
 	template<>
-	constexpr imagedata_t ImageType<int8_t> = MImage_Type_Bit;
+	struct NumericArrayTypeImpl<int8_t> {
+		static constexpr numericarray_data_t type = MNumericArray_Type_Bit8;
+	};
 	template<>
-	constexpr imagedata_t ImageType<uint8_t> = MImage_Type_Bit8;
+	struct NumericArrayTypeImpl<uint8_t> {
+		static constexpr numericarray_data_t type = MNumericArray_Type_UBit8;
+	};
 	template<>
-	constexpr imagedata_t ImageType<uint16_t> = MImage_Type_Bit16;
+	struct NumericArrayTypeImpl<int16_t> {
+		static constexpr numericarray_data_t type = MNumericArray_Type_Bit16;
+	};
 	template<>
-	constexpr imagedata_t ImageType<float> = MImage_Type_Real32;
+	struct NumericArrayTypeImpl<uint16_t> {
+		static constexpr numericarray_data_t type = MNumericArray_Type_UBit16;
+	};
 	template<>
-	constexpr imagedata_t ImageType<double> = MImage_Type_Real;
+	struct NumericArrayTypeImpl<int32_t> {
+		static constexpr numericarray_data_t type = MNumericArray_Type_Bit32;
+	};
+	template<>
+	struct NumericArrayTypeImpl<uint32_t> {
+		static constexpr numericarray_data_t type = MNumericArray_Type_UBit32;
+	};
+	template<>
+	struct NumericArrayTypeImpl<int64_t> {
+		static constexpr numericarray_data_t type = MNumericArray_Type_Bit64;
+	};
+	template<>
+	struct NumericArrayTypeImpl<uint64_t> {
+		static constexpr numericarray_data_t type = MNumericArray_Type_UBit64;
+	};
+	template<>
+	struct NumericArrayTypeImpl<float> {
+		static constexpr numericarray_data_t type = MNumericArray_Type_Real32;
+	};
+	template<>
+	struct NumericArrayTypeImpl<double> {
+		static constexpr numericarray_data_t type = MNumericArray_Type_Real64;
+	};
+	template<>
+	struct NumericArrayTypeImpl<std::complex<float>> {
+		static constexpr numericarray_data_t type = MNumericArray_Type_Complex_Real32;
+	};
+	template<>
+	struct NumericArrayTypeImpl<std::complex<double>> {
+		static constexpr numericarray_data_t type = MNumericArray_Type_Complex_Real64;
+	};
 	/// @endcond
 
 	/// Utility structure that matches a C++ type with a corresponding MNumericArray data type
 	template<typename T>
-	constexpr numericarray_data_t NumericArrayType = MNumericArray_Type_Undef;
+	constexpr numericarray_data_t NumericArrayType = NumericArrayTypeImpl<T>::type;
 
 	/// @cond
+	template<typename T>
+	struct TensorTypeImpl {
+		static constexpr mint type = MType_Undef;
+	};
 	template<>
-	constexpr numericarray_data_t NumericArrayType<int8_t> = MNumericArray_Type_Bit8;
+	struct TensorTypeImpl<mint> {
+		static constexpr mint type = MType_Integer;
+	};
 	template<>
-	constexpr numericarray_data_t NumericArrayType<uint8_t> = MNumericArray_Type_UBit8;
+	struct TensorTypeImpl<double> {
+		static constexpr mint type = MType_Real;
+	};
 	template<>
-	constexpr numericarray_data_t NumericArrayType<int16_t> = MNumericArray_Type_Bit16;
-	template<>
-	constexpr numericarray_data_t NumericArrayType<uint16_t> = MNumericArray_Type_UBit16;
-	template<>
-	constexpr numericarray_data_t NumericArrayType<int32_t> = MNumericArray_Type_Bit32;
-	template<>
-	constexpr numericarray_data_t NumericArrayType<uint32_t> = MNumericArray_Type_UBit32;
-	template<>
-	constexpr numericarray_data_t NumericArrayType<int64_t> = MNumericArray_Type_Bit64;
-	template<>
-	constexpr numericarray_data_t NumericArrayType<uint64_t> = MNumericArray_Type_UBit64;
-	template<>
-	constexpr numericarray_data_t NumericArrayType<float> = MNumericArray_Type_Real32;
-	template<>
-	constexpr numericarray_data_t NumericArrayType<double> = MNumericArray_Type_Real64;
-	template<>
-	constexpr numericarray_data_t NumericArrayType<std::complex<float>> = MNumericArray_Type_Complex_Real32;
-	template<>
-	constexpr numericarray_data_t NumericArrayType<std::complex<double>> = MNumericArray_Type_Complex_Real64;
+	struct TensorTypeImpl<std::complex<double>> {
+		static constexpr mint type = MType_Complex;
+	};
 	/// @endcond
 
 	/// Utility structure that matches a C++ type with a corresponding MTensor data type
 	template<typename T>
-	constexpr mint TensorType = MType_Undef;
+	constexpr mint TensorType = TensorTypeImpl<T>::type;
 
-	/// @cond
-	template<>
-	constexpr mint TensorType<mint> = MType_Integer;
-	template<>
-	constexpr mint TensorType<double> = MType_Real;
-	template<>
-	constexpr mint TensorType<std::complex<double>> = MType_Complex;
-	/// @endcond
 } /* namespace LLU */
 
 #endif /* LLUTILS_UTILITIES_H_ */
