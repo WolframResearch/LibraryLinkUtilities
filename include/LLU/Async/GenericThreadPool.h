8aab0c5cb4e8f9f224bd450cc7d3fa90520466b3
Rafal Chojna <rafalc@wolfram.com>
2019-12-17 15:06:52 +0100

    Finish integrating the thread pool implementation from Williams' book.

---
+++ include/LLU/Async/GenericThreadPool.h
@@ -0,0 +1,199 @@
+/**
+ * @file
+ * @author	Rafal Chojna <rafalc@wolfram.com>
+ * @brief   Definition of the ThreadPool class.
+ */
+
+#ifndef LLU_ASYNC_THREADPOOL_H
+#define LLU_ASYNC_THREADPOOL_H
+
+#include <atomic>
+#include <deque>
+#include <functional>
+#include <future>
+#include <thread>
+#include <vector>
+
+#include "LLU/Async/Queue.h"
+
+namespace LLU {
+
+	class FunctionWrapper {
+		struct TypeErasedCallableBase {
+			virtual void call() = 0;
+			virtual ~TypeErasedCallableBase() = default;
+		};
+
+		template<typename F>
+		struct TypeErasedCallable : TypeErasedCallableBase {
+			explicit TypeErasedCallable(F&& f) : callable(std::forward<F>(f)) {}
+
+			void call() override {
+				callable();
+			}
+			F callable;
+		};
+
+	public:
+		template<typename F>
+		explicit FunctionWrapper(F&& f) : impl{std::make_unique<TypeErasedCallable<F>>(std::forward<F>(f))} {}
+
+		template<typename F, typename... Args>
+		explicit FunctionWrapper(F&& f, Args&&... args) {
+			auto boundF = std::bind(std::forward<F>(f), std::forward<Args>(args)...);
+			impl = std::make_unique<TypeErasedCallable<F>>(std::move(boundF));
+		}
+
+		FunctionWrapper() = default;
+		FunctionWrapper(FunctionWrapper&& other) noexcept = default;
+		FunctionWrapper& operator=(FunctionWrapper&& other) noexcept = default;
+		FunctionWrapper(const FunctionWrapper&) = delete;
+		FunctionWrapper& operator=(const FunctionWrapper&) = delete;
+
+		void operator()() {
+			impl->call();
+		}
+
+	private:
+		std::unique_ptr<TypeErasedCallableBase> impl;
+	};
+
+	template<typename BaseQueue>
+	class WorkStealingQueue {
+	private:
+		using DataType = typename BaseQueue::value_type;
+		BaseQueue theQueue;
+		mutable std::mutex theMutex;
+
+	public:
+		WorkStealingQueue() = default;
+		WorkStealingQueue(const WorkStealingQueue& other) = delete;
+		WorkStealingQueue& operator=(const WorkStealingQueue& other) = delete;
+
+		void push(DataType data) {
+			std::lock_guard<std::mutex> lock(theMutex);
+			theQueue.push_front(std::move(data));
+		}
+		bool empty() const {
+			std::lock_guard<std::mutex> lock(theMutex);
+			return theQueue.empty();
+		}
+		bool tryPop(DataType& res) {
+			std::lock_guard<std::mutex> lock(theMutex);
+			if (theQueue.empty()) {
+				return false;
+			}
+			res = std::move(theQueue.front());
+			theQueue.pop_front();
+			return true;
+		}
+		bool trySteal(DataType& res) {
+			std::lock_guard<std::mutex> lock(theMutex);
+			if (theQueue.empty()) {
+				return false;
+			}
+			res = std::move(theQueue.back());
+			theQueue.pop_back();
+			return true;
+		}
+	};
+
+	class ThreadJoiner {
+		std::vector<std::thread>& threads;
+
+	public:
+		explicit ThreadJoiner(std::vector<std::thread>& threadsToJoin) : threads(threadsToJoin) {}
+		~ThreadJoiner() {
+			for (auto& t : threads) {
+				if (t.joinable()) {
+					t.join();
+				}
+			}
+		}
+	};
+
+	template<typename PoolQueue, typename LocalQueue>
+	class GenericThreadPool {
+	public:
+		using TaskType = typename PoolQueue::value_type;
+
+	public:
+		GenericThreadPool() : GenericThreadPool(std::thread::hardware_concurrency()) {};
+
+		explicit GenericThreadPool(unsigned threadCount) : done(false), joiner(threads) {
+			try {
+				queues.resize(threadCount);
+				for (unsigned i = 0; i < threadCount; ++i) {
+					threads.emplace_back(&GenericThreadPool::workerThread, this, i);
+				}
+			} catch (...) {
+				done = true;
+				throw;
+			}
+		}
+		~GenericThreadPool() {
+			done = true;
+		}
+		template<typename FunctionType>
+		std::future<std::result_of_t<FunctionType()>> submit(FunctionType f) {
+			using result_type = std::result_of_t<FunctionType()>;
+			std::packaged_task<result_type()> task(f);
+			std::future<result_type> res(task.get_future());
+			if (localWorkQueue) {
+				localWorkQueue->push(TaskType {std::move(task)});
+			} else {
+				poolWorkQueue.push(TaskType {std::move(task)});
+			}
+			return res;
+		}
+		void runPendingTask() {
+			TaskType task;
+			if (popTaskFromLocalQueue(task) || popTaskFromPoolQueue(task) || popTaskFromOtherThreadQueue(task)) {
+				task();
+			} else {
+				std::this_thread::yield();
+			}
+		}
+
+	private:
+		std::atomic_bool done;
+		PoolQueue poolWorkQueue;
+		std::vector<std::unique_ptr<LocalQueue>> queues;
+		std::vector<std::thread> threads;
+		ThreadJoiner joiner;
+		static thread_local LocalQueue* localWorkQueue;
+		static thread_local unsigned myIndex;
+		void workerThread(unsigned my_index_) {
+			myIndex = my_index_;
+			localWorkQueue = queues[myIndex].get();
+			while (!done) {
+				runPendingTask();
+			}
+		}
+		bool popTaskFromLocalQueue(TaskType& task) {
+			return localWorkQueue && localWorkQueue->tryPop(task);
+		}
+		bool popTaskFromPoolQueue(TaskType& task) {
+			return poolWorkQueue.tryPop(task);
+		}
+		bool popTaskFromOtherThreadQueue(TaskType& task) {
+			for (unsigned i = 0; i < queues.size(); ++i) {
+				unsigned const index = (myIndex + i + 1) % queues.size();
+				if (queues[index]->trySteal(task)) {
+					return true;
+				}
+			}
+			return false;
+		}
+	};
+
+	template<typename PoolQueue, typename LocalQueue>
+	thread_local LocalQueue* GenericThreadPool<PoolQueue, LocalQueue>::localWorkQueue = nullptr;
+
+	template<typename PoolQueue, typename LocalQueue>
+	thread_local unsigned GenericThreadPool<PoolQueue, LocalQueue>::myIndex = 0;
+
+	using ThreadPool = GenericThreadPool<ThreadsafeQueue<FunctionWrapper>, WorkStealingQueue<std::deque<FunctionWrapper>>>;
+}
+
+#endif	  // LLU_ASYNC_THREADPOOL_H
--- include/LLU/Async/Queue.h
+++ include/LLU/Async/Queue.h
@@ -13,85 +13,88 @@
 namespace LLU {
 
 	template<typename T>
-	class threadsafe_queue {
+	class ThreadsafeQueue {
+	public:
+		using value_type = T;
+
+	public:
+		ThreadsafeQueue() : head(new Node), tail(head.get()) {}
+		ThreadsafeQueue(const ThreadsafeQueue& other) = delete;
+		ThreadsafeQueue& operator=(const ThreadsafeQueue& other) = delete;
+
+		std::shared_ptr<value_type> tryPop();
+		bool tryPop(value_type& value);
+		std::shared_ptr<value_type> waitPop();
+		void waitPop(value_type& value);
+		void push(value_type new_value);
+		bool empty();
+
 	private:
-		struct node {
-			std::shared_ptr<T> data;
-			std::unique_ptr<node> next;
+		struct Node {
+			std::shared_ptr<value_type> data;
+			std::unique_ptr<Node> next;
 		};
 
 		std::mutex head_mutex;
-		std::unique_ptr<node> head;
+		std::unique_ptr<Node> head;
 		std::mutex tail_mutex;
-		node* tail;
+		Node* tail;
 		std::condition_variable data_cond;
 
-		node* get_tail() {
+		Node* getTail() {
 			std::lock_guard<std::mutex> tail_lock(tail_mutex);
 			return tail;
 		}
 
-		std::unique_ptr<node> pop_head() {
-			std::unique_ptr<node> const old_head = std::move(head);
+		std::unique_ptr<Node> popHead() {
+			std::unique_ptr<Node> old_head = std::move(head);
 			head = std::move(old_head->next);
 			return old_head;
 		}
 
-		std::unique_lock<std::mutex> wait_for_data() {
+		std::unique_lock<std::mutex> waitForData() {
 			std::unique_lock<std::mutex> head_lock(head_mutex);
-			data_cond.wait(head_lock, [&] { return head != get_tail(); });
+			data_cond.wait(head_lock, [&] { return head != getTail(); });
 			return std::move(head_lock);
 		}
 
-		std::unique_ptr<node> wait_pop_head() {
-			std::unique_lock<std::mutex> head_lock(wait_for_data());
-			return pop_head();
+		std::unique_ptr<Node> waitPopHead() {
+			std::unique_lock<std::mutex> head_lock(waitForData());
+			return popHead();
 		}
 
-		std::unique_ptr<node> wait_pop_head(T& value) {
-			std::unique_lock<std::mutex> head_lock(wait_for_data());
+		std::unique_ptr<Node> waitPopHead(value_type& value) {
+			std::unique_lock<std::mutex> head_lock(waitForData());
 			value = std::move(*head->data);
-			return pop_head();
+			return popHead();
 		}
 
-		std::unique_ptr<node> try_pop_head() {
+		std::unique_ptr<Node> tryPopHead() {
 			std::lock_guard<std::mutex> head_lock(head_mutex);
-			if (head.get() == get_tail()) {
-				return std::unique_ptr<node>();
+			if (head.get() == getTail()) {
+				return std::unique_ptr<Node>();
 			}
-			return pop_head();
+			return popHead();
 		}
 
-		std::unique_ptr<node> try_pop_head(T& value) {
+		std::unique_ptr<Node> tryPopHead(value_type& value) {
 			std::lock_guard<std::mutex> head_lock(head_mutex);
-			if (head.get() == get_tail()) {
-				return std::unique_ptr<node>();
+			if (head.get() == getTail()) {
+				return std::unique_ptr<Node>();
 			}
 			value = std::move(*head->data);
-			return pop_head();
+			return popHead();
 		}
-
-	public:
-		threadsafe_queue() : head(new node), tail(head.get()) {}
-		threadsafe_queue(const threadsafe_queue& other) = delete;
-		threadsafe_queue& operator=(const threadsafe_queue& other) = delete;
-
-		std::shared_ptr<T> try_pop();
-		bool try_pop(T& value);
-		std::shared_ptr<T> wait_and_pop();
-		void wait_and_pop(T& value);
-		void push(T new_value);
-		bool empty();
 	};
 
 	template<typename T>
-	void threadsafe_queue<T>::push(T new_value) {
+	void ThreadsafeQueue<T>::push(T new_value) {
 		std::shared_ptr<T> new_data(std::make_shared<T>(std::move(new_value)));
-		std::unique_ptr<node> p(new node);
+		std::unique_ptr<Node> p(new Node);
 		{
 			std::lock_guard<std::mutex> tail_lock(tail_mutex);
 			tail->data = new_data;
-			node* const new_tail = p.get();
+			Node* const new_tail = p.get();
 			tail->next = std::move(p);
 			tail = new_tail;
 		}
@@ -99,32 +102,32 @@ namespace LLU {
 	}
 
 	template<typename T>
-	std::shared_ptr<T> threadsafe_queue<T>::wait_and_pop() {
-		std::unique_ptr<node> const old_head = wait_pop_head();
+	std::shared_ptr<T> ThreadsafeQueue<T>::waitPop() {
+		std::unique_ptr<Node> const old_head = waitPopHead();
 		return old_head->data;
 	}
 
 	template<typename T>
-	void threadsafe_queue<T>::wait_and_pop(T& value) {
-		std::unique_ptr<node> const old_head = wait_pop_head(value);
+	void ThreadsafeQueue<T>::waitPop(T& value) {
+		std::unique_ptr<Node> const old_head = waitPopHead(value);
 	}
 
 	template<typename T>
-	std::shared_ptr<T> threadsafe_queue<T>::try_pop() {
-		std::unique_ptr<node> const old_head = try_pop_head();
+	std::shared_ptr<T> ThreadsafeQueue<T>::tryPop() {
+		std::unique_ptr<Node> const old_head = tryPopHead();
 		return old_head ? old_head->data : std::shared_ptr<T>();
 	}
 
 	template<typename T>
-	bool threadsafe_queue<T>::try_pop(T& value) {
-		std::unique_ptr<node> const old_head = try_pop_head(value);
-		return old_head;
+	bool ThreadsafeQueue<T>::tryPop(T& value) {
+		std::unique_ptr<Node> const old_head = tryPopHead(value);
+		return static_cast<bool>(old_head);
 	}
 
 	template<typename T>
-	bool threadsafe_queue<T>::empty() {
+	bool ThreadsafeQueue<T>::empty() {
 		std::lock_guard<std::mutex> head_lock(head_mutex);
-		return (head == get_tail());
+		return (head == getTail());
 	}
 }
 #endif	  // LLU_ASYNC_QUEUE_H
