160f32f720131a7713711e119f180c795709ad9a
Rafal Chojna <rafalc@wolfram.com>
2020-01-15 19:56:36 +0100

    Fix the behavior of ThreadsafeQueue if it is destructed when there are threads waiting on the conditional variable.

--- include/LLU/Async/Queue.h
+++ include/LLU/Async/Queue.h
@@ -6,6 +6,7 @@
 #ifndef LLU_ASYNC_QUEUE_H
 #define LLU_ASYNC_QUEUE_H
 
+#include <atomic>
 #include <condition_variable>
 #include <memory>
 #include <mutex>
@@ -19,13 +20,17 @@ namespace LLU {
 
 	public:
 		ThreadsafeQueue() : head(new Node), tail(head.get()) {}
+		~ThreadsafeQueue() {
+			done = true;
+			data_cond.notify_all();
+		}
 		ThreadsafeQueue(const ThreadsafeQueue& other) = delete;
 		ThreadsafeQueue& operator=(const ThreadsafeQueue& other) = delete;
 
 		std::shared_ptr<value_type> tryPop();
 		bool tryPop(value_type& value);
 		std::shared_ptr<value_type> waitPop();
-		void waitPop(value_type& value);
+		bool waitPop(value_type& value);
 		void push(value_type new_value);
 		bool empty();
 
@@ -35,6 +40,7 @@ namespace LLU {
 			std::unique_ptr<Node> next;
 		};
 
+		std::atomic_bool done = false;
 		std::mutex head_mutex;
 		std::unique_ptr<Node> head;
 		std::mutex tail_mutex;
@@ -47,6 +53,9 @@ namespace LLU {
 		}
 
 		std::unique_ptr<Node> popHead() {
+			if (head.get() == getTail()) {
+				return nullptr;
+			}
 			std::unique_ptr<Node> old_head = std::move(head);
 			head = std::move(old_head->next);
 			return old_head;
@@ -54,7 +63,7 @@ namespace LLU {
 
 		std::unique_lock<std::mutex> waitForData() {
 			std::unique_lock<std::mutex> head_lock(head_mutex);
-			data_cond.wait(head_lock, [&] { return head.get() != getTail(); });
+			data_cond.wait(head_lock, [&] { return done || head.get() != getTail(); });
 			return std::move(head_lock);
 		}
 
@@ -65,7 +74,9 @@ namespace LLU {
 
 		std::unique_ptr<Node> waitPopHead(value_type& value) {
 			std::unique_lock<std::mutex> head_lock(waitForData());
-			value = std::move(*head->data);
+			if (head->data) {
+				value = std::move(*head->data);
+			}
 			return popHead();
 		}
 
@@ -104,12 +115,13 @@ namespace LLU {
 	template<typename T>
 	std::shared_ptr<T> ThreadsafeQueue<T>::waitPop() {
 		std::unique_ptr<Node> const old_head = waitPopHead();
-		return old_head->data;
+		return old_head? old_head->data : nullptr;
 	}
 
 	template<typename T>
-	void ThreadsafeQueue<T>::waitPop(T& value) {
+	bool ThreadsafeQueue<T>::waitPop(T& value) {
 		std::unique_ptr<Node> const old_head = waitPopHead(value);
+		return old_head != nullptr;
 	}
 
 	template<typename T>
