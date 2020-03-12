9ab9e89bb20fa9b96d5b20c8ce4e297cce36d40a
Rafal Chojna <rafalc@wolfram.com>
2020-03-12 13:25:48 +0100

    Document Async utilities.

--- include/LLU/Async/Utilities.h
+++ include/LLU/Async/Utilities.h
@@ -14,6 +14,10 @@
 
 namespace LLU::Async {
 
+	/**
+	 * @class FunctionWrapper
+	 * @brief Wraps an arbitrary callable object (possibly binding its arguments) to be evaluated later.
+	 */
 	class FunctionWrapper {
 		struct TypeErasedCallableBase {
 			virtual void call() = 0;
@@ -54,6 +58,13 @@ namespace LLU::Async {
 		std::unique_ptr<TypeErasedCallableBase> impl = nullptr;
 	};
 
+	/**
+	 * @class ThreadJoiner
+	 * @brief A guard for a vector of threads to make sure they are joined before their destruction.
+	 * The developer must ensure that the lifetime a ThreadJoiner does not exceed the lifetime of the vector it guards.
+	 *
+	 * @note This class may get deprecated in favor of std::jthread when LLU switches to C++20.
+	 */
 	class ThreadJoiner {
 		std::vector<std::thread>& threads;
 
@@ -68,6 +79,14 @@ namespace LLU::Async {
 		}
 	};
 
+	/**
+	 * Create a std::packaged_task from a callable object and arguments to it.
+	 * @tparam FunctionType - arbitrary type of a callable object
+	 * @tparam Args - paramater pack with function argument types
+	 * @param f - callable object
+	 * @param args - arguments for a call to \p f
+	 * @return a call to \p f with arguments \p args wrapped in a std::packaged_task
+	 */
 	template<typename FunctionType, typename... Args>
 	std::packaged_task<std::invoke_result_t<FunctionType, Args...>()> getPackagedTask(FunctionType&& f, Args&&... args) {
 		using result_type = std::invoke_result_t<FunctionType, Args...>;
@@ -75,6 +94,10 @@ namespace LLU::Async {
 		return std::packaged_task<result_type()> {std::move(boundF)};
 	}
 
+	/**
+	 * @class Pausable
+	 * @brief Utility class for pausable task queues.
+	 */
 	class Pausable {
 		std::atomic_bool pausedQ = false;
 		std::mutex workersMutex;
