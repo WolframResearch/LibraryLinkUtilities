bb93eb56beef9eb936dfa62a52b4e30be6a890a9
Rafal Chojna <rafalc@wolfram.com>
2020-05-18 17:12:17 +0200

    Update compilation error tests for containers.

--- tests/UnitTests/GenericContainers/TestSources/PoliciesCompilationErrors.cpp
+++ tests/UnitTests/GenericContainers/TestSources/PoliciesCompilationErrors.cpp
@@ -8,34 +8,6 @@
 
 using LLU::Passing;
 
-LIBRARY_LINK_FUNCTION(CreateAutomatic) {
-	LLU::NumericArray<uint8_t, Automatic> na {1, 2, 3, 4, 5};
-	return 0;
-}
-
-LIBRARY_LINK_FUNCTION(CreateShared) {
-	LLU::NumericArray<uint8_t, Shared> na {1, 2, 3, 4, 5};
-	return 0;
-}
-
-LIBRARY_LINK_FUNCTION(SharedGenericAsManual) {
-	LLU::MArgumentManager mngr {libData, Argc, Args, Res};
-	LLU::Tensor<mint, Manual> t = mngr.getGenericTensor<Shared>(0);
-	return 0;
-}
-
-LIBRARY_LINK_FUNCTION(SharedAsAutomatic) {
-	LLU::MArgumentManager mngr {libData, Argc, Args, Res};
-	LLU::GenericTensor<Automatic> t = mngr.getGenericTensor<Shared>(0);
-	return 0;
-}
-
-LIBRARY_LINK_FUNCTION(AutomaticAsShared) {
-	LLU::MArgumentManager mngr {libData, Argc, Args, Res};
-	LLU::GenericTensor<Shared> t = mngr.getGenericTensor<>(0);
-	return 0;
-}
-
 LIBRARY_LINK_FUNCTION(DisownManual) {
 	LLU::NumericArray<uint8_t> na {1, 2, 3, 4, 5};
 	na.disown();
@@ -59,13 +31,6 @@ LIBRARY_LINK_FUNCTION(CopyShared) {
 LIBRARY_LINK_FUNCTION(SharedDataStore) {
 	LLU::MArgumentManager mngr {libData, Argc, Args, Res};
 	auto ds = mngr.getGenericDataList<Passing::Shared>(0);
-	return 0;
-}
-
-LIBRARY_LINK_FUNCTION(MoveAutomatic) {
-	LLU::MArgumentManager mngr {libData, Argc, Args, Res};
-	auto img = mngr.getGenericImage(0);
-	LLU::GenericImage clone = std::move(img);	 // cannot move Automatic to Manual
 	return 0;
 }
 
