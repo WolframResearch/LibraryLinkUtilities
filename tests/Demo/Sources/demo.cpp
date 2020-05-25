e8ae72f73288555b097645b72fad0bada4edfa8e
Rafal Chojna <rafalc@wolfram.com>
2020-05-25 18:57:22 +0200

    Add two simple functions to the Demo paclet: Caesar cipher encoding and decoding.

--- tests/Demo/Sources/demo.cpp
+++ tests/Demo/Sources/demo.cpp
@@ -9,11 +9,57 @@
 EXTERN_C DLLEXPORT int WolframLibrary_initialize(WolframLibraryData libData) {
 	try {
 		LLU::LibraryData::setLibraryData(libData);
-//		LLU::ErrorManager::registerPacletErrors({
-//			{"MatioIncorrectLibraryVersion", "matio header version is `hv`, but paclet linked to version `pv`."},
-//			{"UnexpectedMArgumentType", "Unexpected MArgument type: `type`."},
-//		});
+		LLU::ErrorManager::registerPacletErrors({
+			{"InvalidCharacterError", "Message \"`m`\" contains non-ASCII character(s)."},
+			{"NegativeShiftError", "Requested negative shift `s`."},
+		});
 
+	} catch (const LLU::LibraryLinkError& e) {
+		return e.which();
+	}
+	return LLU::ErrorCode::NoError;
+}
+
+namespace {
+	constexpr std::string_view alphabet {"ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"};
+}
+
+std::string performCaesarCipher(const std::string& message, mint shift) {
+	if (shift < 0) {
+		LLU::ErrorManager::throwException("NegativeShiftError", shift);
+	}
+
+	auto resultStr {message};
+	for (auto& c : resultStr) {
+		auto index = alphabet.find(c);
+		if (index == std::string::npos) {
+			LLU::ErrorManager::throwException("InvalidCharacterError", message);
+		}
+		c = alphabet[(index + shift) % alphabet.length()];
+	}
+	return resultStr;
+}
+
+EXTERN_C DLLEXPORT int CaesarCipherEncode(WolframLibraryData libData, mint Argc, MArgument *Args, MArgument Res) {
+	try {
+		LLU::MArgumentManager mngr {libData, Argc, Args, Res};
+		auto str = mngr.getString(0);
+		const auto shift = mngr.getInteger<mint>(1);
+		auto result = performCaesarCipher(str, shift);
+		mngr.set(result);
+	} catch (const LLU::LibraryLinkError& e) {
+		return e.which();
+	}
+	return LLU::ErrorCode::NoError;
+}
+
+EXTERN_C DLLEXPORT int CaesarCipherDecode(WolframLibraryData libData, mint Argc, MArgument *Args, MArgument Res) {
+	try {
+		LLU::MArgumentManager mngr {libData, Argc, Args, Res};
+		auto str = mngr.getString(0);
+		const auto shift = mngr.getInteger<mint>(1);
+		auto result = performCaesarCipher(str, alphabet.length() - shift);
+		mngr.set(result);
 	} catch (const LLU::LibraryLinkError& e) {
 		return e.which();
 	}
--- tests/Demo/Tests/test.wl
+++ tests/Demo/Tests/test.wl
@@ -1,3 +1,40 @@
-Needs["Demo`"]
+Needs["Demo`"];
+Needs["MUnit`"];
 
-Print @ Demo`Squared[12];
+$tests = Hold @ {
+	Test[
+		Demo`CaesarCipherDecode::usage
+		,
+		"CaesarCipherDecode[cipherText_String, shift_Integer] restores the original message encoded with Caesar's cipher given the encoded text and the shift."
+	],
+
+	Test[
+		Demo`CaesarCipherDecode[Demo`CaesarCipherEncode["HelloWorld", 3], 3]
+		,
+		"HelloWorld"
+	],
+
+	Test[
+		Demo`CaesarCipherDecode[Demo`CaesarCipherEncode["HelloWorld", 25], 25]
+		,
+		"HelloWorld"
+	],
+
+	VerificationTest[
+		f = Demo`CaesarCipherEncode["HelloWorld", -1];
+		FailureQ[f] && First[f] === "NegativeShiftError"
+	],
+
+	VerificationTest[
+		f = Demo`CaesarCipherEncode["HelloWorld", -1];
+		FailureQ[f] && First[f] === "NegativeShiftError"
+	]
+};
+
+TestRun[
+	Evaluate @ $tests,
+	Loggers :> {VerbosePrintLogger[]},
+	TestRunTitle -> "Basic test suite of the Demo paclet",
+	MemoryConstraint -> Quantity[100, "Megabytes"],
+	TimeConstraint -> Quantity[60, "Seconds"]
+]
